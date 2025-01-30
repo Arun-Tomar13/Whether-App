c2461f9b67b3274c66d1636dcfa2c1fbf30b3079
Arun-Tomar13 <aruntomar1330@gmail.com>
2025-01-30 20:36:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey:"Your apiKey";
+const apiKey:"Your API_Key(apiKey)";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
    const searchBox= document.querySelector(".search input");
